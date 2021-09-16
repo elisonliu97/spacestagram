4d0f4a21eb815f3c7b0ce921aaee405213021b11
Elison <elisonliu97@gmail.com>
2021-09-16 15:44:03 -0400

    used different parameters for nasa api and added pagination

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Card from './components/card/card';
 import Masonry from 'react-masonry-css';
 import './App.css';
@@ -7,13 +7,35 @@ function App() {
 
   const [apiData, setApiData] = useState([])
   const [loading, setLoading] = useState(false)
+  const [likes, setLikes] = useState([])
 
-  let date1
-  let date2
+  useEffect(() => {
+    let likeArr = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      likeArr.push(localStorage.key[i])
+    }
 
-  let apiURL = `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}&start_date=${date1}&end_date=${date2}`
+    setLikes(likeArr)
+    getPictures();
+  }, [])
+
+  let apiURL = `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}&count=15`
+
+  function applyEventListener() {
+    const scrolling_function = () => {
+      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 10) {
+        console.log("fetching more.........")
+        window.removeEventListener('scroll', scrolling_function)
+        getPictures();
+      }
+    }
+    if (!loading) {
+      window.addEventListener('scroll', scrolling_function);
+    }
+  }
 
   async function getPictures() {
+    setLoading(true)
     console.log('here')
     try {
       const response = await fetch(apiURL);
@@ -23,9 +45,11 @@ function App() {
       }
 
       const items = await response.json();
-      setApiData(items)
+      const newApiData = [...apiData, ...items]
       setLoading(false)
-      console.log(items)
+      console.log(newApiData)
+      setApiData(apiData => [...apiData, ...items])
+      applyEventListener()
 
     } catch (err) {
       setLoading(false)
@@ -39,48 +63,26 @@ function App() {
     800: 1
   }
 
-  function submitHandler() {
-    date1 = document.querySelector("#date-picker-1").value
-    date2 = document.querySelector("#date-picker-2").value
-
-    if (!date1 || !date2) {
-      alert("please enter proper dates")
-      return
-    }
-
-    if (date1 > date2) {
-      alert('please have your end date after your start date')
-      return
-    }
-
-    apiURL = `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}&start_date=${date1}&end_date=${date2}`
-    setLoading(true)
-    getPictures();
-  }
-
   return (
     <div className="App">
-      <input id="date-picker-1" type="date"></input>
-      <input id="date-picker-2" type="date"></input>
-      <button onClick={() => submitHandler()}
-      disabled={loading}>SUBMIT</button>
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="card-container"
+        columnClassName="my-masonry-grid_column"
+      >
+        {apiData.map((item) => {
+          return (
+            <Card key={item.date} item={item} likes={likes} />
+          )
+        })}
+
 
       {(loading ?
         <div>Loading</div>
         :
-
-        <Masonry
-          breakpointCols={breakpointColumnsObj}
-          className="card-container"
-          columnClassName="my-masonry-grid_column"
-        >
-          {apiData.map((item) => {
-            return (
-              <Card key={item.date} item={item} />
-            )
-          })}
-        </Masonry>
+        null
       )}
+            </Masonry>
     </div>
   );
 }
