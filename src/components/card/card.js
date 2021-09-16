24ddd6b2de395a186cdef4cafd8232846cbca371
Elison <elisonliu97@gmail.com>
2021-09-15 23:56:39 -0400

    share feature

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -30,6 +30,15 @@ function Card(prop) {
         }
     }
 
+    function saveToClipboard() {
+        navigator.clipboard.writeText(prop.item.url);
+        alert("Copied Link")
+    }
+
+    function likeFunction() {
+        localStorage.setItem(prop.item.date, prop.item.title)
+    }
+
     return (
         <div className="card-div">
             {mediaType(prop)}
@@ -37,6 +46,8 @@ function Card(prop) {
                     <p>{prop.item.title}</p>
                     <p>{prop.item.date}</p>
                     <p>{prop.item.explanation}</p>
+                    <button onClick={() => likeFunction()}>Like</button>
+                    <button onClick={() => saveToClipboard()}>Share</button>
                 </div>
         </div>
     )
