85ba7f8db0a3a80cf88a3dae7731d39c11d05e3d
Elison <elisonliu97@gmail.com>
2021-09-17 15:13:03 -0400

    logo

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,11 +1,13 @@
 import rocket from './rocket.png';
 import heart from './heart.png';
 import clipboard from './clipboard.png'
+import logo from './space-logo.png'
 
 const imgs = {
     rocket,
     heart,
-    clipboard
+    clipboard,
+    logo,
 }
 
 export default imgs;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,13 @@
-
+import imgs from '../../assets/index';
 import './header.css';
 
 function Header() {
     return (
         <div className="header-container">
-            <h1 className="header-title"><span className="title-font">Space</span>stagram</h1>
+            <h1 className="header-title">
+            <img className="header-icon" src={imgs.logo} alt="logo"/>
+                <span className="title-font">Space</span>stagram
+            </h1>
             <p className="header-underline">Brought to you by NASA's Astronomy Photo of the Day API</p>
         </div>
     )
